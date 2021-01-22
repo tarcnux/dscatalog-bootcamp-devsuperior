5977e7f4d15db77cbc865b920a4d8db5f4d67069
Tarcisio Nunes <github@tarcnux.com.br>
2021-01-22 16:20:24 -0300

    DTO - Data Transfer Object

--- backend/src/main/java/com/tarcnux/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/tarcnux/dscatalog/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.tarcnux.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.tarcnux.dscatalog.dto.CategoryDTO;
 import com.tarcnux.dscatalog.entities.Category;
 import com.tarcnux.dscatalog.repositories.CategoryRepository;
 
@@ -16,7 +18,16 @@ public class CategoryService {
 	private CategoryRepository repository;
 
 	@Transactional(readOnly = true)
-	public List<Category> findAll() {
-		return repository.findAll();
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+		//Converting each object Category into CategoryDTO
+		//First list 2 stream and finally Collectors 2 List Again
+		List<CategoryDTO> listDto = list.stream()
+				.map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+		return listDto;
+		
+		//It's also possible return directly
+		// return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+		
 	}
 }
