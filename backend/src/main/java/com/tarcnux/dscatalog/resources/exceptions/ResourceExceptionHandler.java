6492d397cb93f3b5ba477ce42de3236aea24a0af
Tarcisio Nunes <github@tarcnux.com.br>
2021-01-26 18:09:37 -0300

    Update Category

--- backend/src/main/java/com/tarcnux/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/tarcnux/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -10,13 +10,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import com.tarcnux.dscatalog.services.exceptions.EntityNotFoundException;
+import com.tarcnux.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
 	
-	@ExceptionHandler(EntityNotFoundException.class)
-	public ResponseEntity<StandardError> entityNotFound(EntityNotFoundException e, 
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, 
 			HttpServletRequest request) {
 		StandardError err = new StandardError();
 		err.setTimestamp(Instant.now());
