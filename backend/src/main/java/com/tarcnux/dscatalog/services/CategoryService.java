83cdcd76b4c967f331951df515e92d21d685b01b
Tarcisio Nunes <github@tarcnux.com.br>
2021-01-26 13:54:56 -0300

    POST new category

--- backend/src/main/java/com/tarcnux/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/tarcnux/dscatalog/services/CategoryService.java
@@ -41,4 +41,12 @@ public class CategoryService {
 		Category entity = objOptional.orElseThrow(() -> new EntityNotFoundException("Entity NOT found"));
 		return new CategoryDTO(entity);
 	}
+
+	@Transactional
+	public CategoryDTO insert(CategoryDTO dto) {
+		Category entity = new Category();
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		return new CategoryDTO(entity);
+	}
 }
