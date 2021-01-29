d6a002e9b8a56c55945cd832d39afaa333c8db7a
Tarcisio Nunes <github@tarcnux.com.br>
2021-01-29 02:35:46 -0300

    Produtct insert, update, delete

--- backend/src/main/java/com/tarcnux/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/tarcnux/dscatalog/services/ProductService.java
@@ -1,7 +1,6 @@
 package com.tarcnux.dscatalog.services;
 
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import javax.persistence.EntityNotFoundException;
 
@@ -13,8 +12,11 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.tarcnux.dscatalog.dto.CategoryDTO;
 import com.tarcnux.dscatalog.dto.ProductDTO;
+import com.tarcnux.dscatalog.entities.Category;
 import com.tarcnux.dscatalog.entities.Product;
+import com.tarcnux.dscatalog.repositories.CategoryRepository;
 import com.tarcnux.dscatalog.repositories.ProductRepository;
 import com.tarcnux.dscatalog.services.exceptions.DatabaseException;
 import com.tarcnux.dscatalog.services.exceptions.ResourceNotFoundException;
@@ -24,6 +26,9 @@ public class ProductService {
 	
 	@Autowired
 	private ProductRepository repository;
+	
+	@Autowired 
+	private CategoryRepository categoryRepository;
 
 	@Transactional(readOnly = true)
 	public Page<ProductDTO> findAllPaged(PageRequest pageRequest) {
@@ -51,7 +56,7 @@ public class ProductService {
 	@Transactional
 	public ProductDTO insert(ProductDTO dto) {
 		Product entity = new Product();
-//		entity.setName(dto.getName());
+		copyDtoToEntity(dto, entity);
 		entity = repository.save(entity);
 		return new ProductDTO(entity);
 	}
@@ -60,7 +65,7 @@ public class ProductService {
 	public ProductDTO update(Long id, ProductDTO dto) {
 		try {
 			Product entity = repository.getOne(id);
-//			entity.setName(dto.getName());		
+			copyDtoToEntity(dto, entity);
 			entity = repository.save(entity);
 			return new ProductDTO(entity);
 		} catch(EntityNotFoundException e) {
@@ -75,6 +80,20 @@ public class ProductService {
 			throw new ResourceNotFoundException("Id not found: " + id);
 		} catch (DataIntegrityViolationException e) {
 			throw new DatabaseException("Integrity violation");
+		}
+	}
+	
+	private void copyDtoToEntity(ProductDTO dto, Product entity) {
+		entity.setName(dto.getName());
+		entity.setDescription(dto.getDescription());
+		entity.setDate(dto.getDate());
+		entity.setImgUrl(dto.getImgUrl());
+		entity.setPrice(dto.getPrice());
+		
+		entity.getCategories().clear();
+		for (CategoryDTO catDto : dto.getCategories()) {
+			Category category = categoryRepository.getOne(catDto.getId());
+			entity.getCategories().add(category);
 		}
 	}
 	
