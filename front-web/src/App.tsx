d5c5752f4714affcdaffa530a1f0aae94d73ced9
Tarcisio Nunes <github@tarcnux.com.br>
2021-02-09 19:08:17 -0300

    Re-start project

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,32 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 const App = () => {
     
-    //React Hooks useStsate - Componente com estado
-    const [counter, setCounter] = useState(0);
-
-    //React Hook useEffect - Ciclo de Vida do componente
-    useEffect(() => {
-        console.log("Componente iniciado")
-    }, []);
 
     return (
         <div className="container mt-5">
-            <button className="btn btn-primary mr-5"
-                onClick={() => setCounter(counter + 1)} 
-            >
-                +
-            </button>
-            <span>
-                { counter }
-            </span>
-            <button className="btn btn-danger ml-5"
-                onClick={() => setCounter(counter - 1)} 
-            >
-                -
-            </button>             
-            {counter > 5 && <h2 className="bg-primary text-light">O valor é maior do que 5</h2>}
-            {counter <= 5 && <h2 className="bg-danger text-light">O valor é menor do que 5</h2>}
+            <h1>Alo HA</h1>
         </div>
     );
 }
