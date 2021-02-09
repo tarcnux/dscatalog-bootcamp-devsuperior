7810a324de8acb2b05aade2a719ba2bd5c7f979f
Tarcisio Nunes <github@tarcnux.com.br>
2021-02-09 17:57:17 -0300

    Exemplo de contador em React

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,13 +1,32 @@
-import React from 'react';
-import Alert from './Alert';
+import React, { useState, useEffect } from 'react';
 
 const App = () => {
+    
+    //React Hooks useStsate - Componente com estado
+    const [counter, setCounter] = useState(0);
+
+    //React Hook useEffect - Ciclo de Vida do componente
+    useEffect(() => {
+        console.log("Componente iniciado")
+    }, []);
+
     return (
         <div className="container mt-5">
-            <Alert text=" meu mundão"/>
-            <Alert /> 
-            <Alert />
-            <Alert text=" meu mundululu"/>
+            <button className="btn btn-primary mr-5"
+                onClick={() => setCounter(counter + 1)} 
+            >
+                +
+            </button>
+            <span>
+                { counter }
+            </span>
+            <button className="btn btn-danger ml-5"
+                onClick={() => setCounter(counter - 1)} 
+            >
+                -
+            </button>             
+            {counter > 5 && <h2 className="bg-primary text-light">O valor é maior do que 5</h2>}
+            {counter <= 5 && <h2 className="bg-danger text-light">O valor é menor do que 5</h2>}
         </div>
     );
 }
