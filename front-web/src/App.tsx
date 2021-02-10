9dd64f161f49f3e66ca01e488c58bbce6865b8d3
Tarcisio Nunes <github@tarcnux.com.br>
2021-02-09 22:37:39 -0300

    Add Navbar Component

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './core/assets/styles/custom.scss';
 import './app.scss'
+import Navbar from './core/components/Navbar';
 
 const App = () => {
     
 
     return (
-        <h1>Alo HA</h1>
+        <Navbar />
     );
 }
 
