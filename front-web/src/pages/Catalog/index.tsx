7c0658071eceb4cfd0b8c33d97f66fe8cbe6e85a
Tarcisio Nunes <github@tarcnux.com.br>
2021-02-11 01:07:08 -0300

    Style Catalog Page

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import ProductCard from './Components/ProductCard';
+import './styles.scss';
 
 const Catalog = () => (
-    <div>
-        <h1>Catalog</h1>
+    <div className="catalog-container">
+        <h1 className="catalog-title">Catalogo de produtos</h1>
+        <div className="catalog-products border-radius-10">
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+        </div>
     </div>
 );
 
