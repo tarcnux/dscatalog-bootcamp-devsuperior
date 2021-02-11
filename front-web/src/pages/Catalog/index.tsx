613db67c76eeee9c7e81586b91c74e7dcd923d4f
Tarcisio Nunes <github@tarcnux.com.br>
2021-02-11 17:57:36 -0300

    Fetch Products data from Proxy

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -1,23 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom';
 import ProductCard from './Components/ProductCard';
 import './styles.scss';
 
-const Catalog = () => (
-    <div className="catalog-container">
-        <h1 className="catalog-title">Catalogo de produtos</h1>
-        <div className="catalog-products border-radius-10">
-            <Link to="/products/1"><ProductCard /></Link>
-            <Link to="/products/2"><ProductCard /></Link>
-            <Link to="/products/3"><ProductCard /></Link>
-            <Link to="/products/4"><ProductCard /></Link>
-            <Link to="/products/5"><ProductCard /></Link>
-            <Link to="/products/6"><ProductCard /></Link>
-            <Link to="/products/7"><ProductCard /></Link>
-            <Link to="/products/8"><ProductCard /></Link>
-            <Link to="/products/9"><ProductCard /></Link>
+const Catalog = () => {
+    //quando o componente iniciar, buscar a lista de produtos
+    //quando a lista de produtos estiver disponível,
+    //popular um estado do componente, e listar os produtos dinamicamente
+    useEffect(() => {
+        console.log('Catalog: Componente de listagem de produtos iniciado.')
+        fetch('http://localhost:3000/products')
+        .then(response => response.json())
+        .then(response => console.log(response));
+    }, []);
+
+    return (
+        <div className="catalog-container">
+            <h1 className="catalog-title">Catalogo de produtos</h1>
+            <div className="catalog-products border-radius-10">
+                <Link to="/products/1"><ProductCard /></Link>
+                <Link to="/products/2"><ProductCard /></Link>
+                <Link to="/products/3"><ProductCard /></Link>
+                <Link to="/products/4"><ProductCard /></Link>
+                <Link to="/products/5"><ProductCard /></Link>
+                <Link to="/products/6"><ProductCard /></Link>
+                <Link to="/products/7"><ProductCard /></Link>
+                <Link to="/products/8"><ProductCard /></Link>
+                <Link to="/products/9"><ProductCard /></Link>
+            </div>
         </div>
-    </div>
-);
+    );
+}
 
 export default Catalog;
